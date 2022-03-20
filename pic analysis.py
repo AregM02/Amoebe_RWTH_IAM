af2a36ac6364a3d7ace6788fddd7c6a6721e17b9
AregM02 <areg.melkumyan@rwth-aachen.de>
2022-03-20 18:37:04 +0400

    Performance improved massively

--- pic analysis.py
+++ pic analysis.py
@@ -10,34 +10,23 @@ lower = np.array([22, 93, 0])
 upper = np.array([45, 255, 255])
 bw_image = cv2.inRange(image, lower, upper)
 
-cv2.imwrite('bw picture.jpg', bw_image)
-scan = Image.open("bw picture.jpg")
+data = np.asarray(bw_image)
+non_zero_indices = np.argwhere(data != 0)
+Xc = non_zero_indices[:, 0].sum()
+Yc = non_zero_indices[:, 1].sum()
+Area = non_zero_indices.shape[0]
 
-x = 0
-Area = 0
-Xc = 0
-Yc = 0
-while x < scan.width:
-    y = 0
-    while y < scan.height:
-        pixel_value = scan.getpixel((x, y))
-        if pixel_value == 255:
-            Area += 1
-            Xc += x
-            Yc += y
-        y += 1
-    x += 1
-
-Xc = Xc/Area+(1805*2346)/Area
-Yc = Yc/Area+(2346*1098)/Area
+# Correction and calculation
+Xc = Xc / Area + (1805 * 2346) / Area
+Yc = Yc / Area + (2346 * 1098) / Area
 print(Xc, Yc)
 
 new = Image.open("Amoebe.jpg")
 image_editable = ImageDraw(new)
-image_editable.text((Xc - 254, Yc - 65),'''           
+image_editable.text((Xc - 254, Yc - 65), '''
                                           |
                                           |
-                                          | 
+                                          |
                                  ---------|---------
                                           |
                                           |
